fe816268805ef14c069c62b59f727492397dbd56
Gracrys <gracrys@gmail.com>
2020-07-16 21:03:34 -0430

    added remove first function

--- list.fs
+++ list.fs
@@ -6,7 +6,15 @@ type MyType = {N : int}
 type MyList = int list
 
 let max x y = if x < y then y else x
+let min x y = if x > y then y else x
 
 let maxList (x:int list) = List.reduce max x
+let minList (x:int list) = List.reduce min x
 
-let rec removeFst n x = List.filter (fun y -> y <> x) n
+let removeFst n x = List.filter (fun y -> y <> x) n
+
+let rec srtInts xs =
+  let m = minList xs
+  match xs with
+  | [] -> []
+  | _ -> m @ srtInts << removeFst m xs 
