e1db2ae65a19ffd7bece913f0952a3d7b2995bfd
Gracrys <gracrys@gmail.com>
2020-07-18 16:52:46 -0430

    find ocurrences of a char in string

--- strings.fs
+++ strings.fs
@@ -5,5 +5,7 @@ type System.String with
     member this.Find x = 
         Seq.toList this |> List.filter (fun y -> x = y) |> List.length
 
+    member this.Replicate = 
+        Seq.toList this |> List.mapi (fun s i -> (*String.replicate (String s)*) s )  
 
-printfn "%A" <| "Hello motto".Find 'l'
+printfn "%A" <| "Hello".Replicate 
